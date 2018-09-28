d0093ff8f1bf81f5b6ff2e1605305df08d3533b6
Pavel Pershko <thrashmetalfan98@gmail.com>
2018-09-28 20:09:33 +0300

    Refactor FileSerializerOutput

--- tracer/FileSerializerOutput.cs
+++ tracer/FileSerializerOutput.cs
@@ -5,13 +5,18 @@ namespace tracer
     public class FileSerializerOutput : ISerializerOutput
     {
         public ISerializer Serializer;
+        public string FileName;
 
-        public FileSerializerOutput(ISerializer serializer) => Serializer = serializer;
+        public FileSerializerOutput(ISerializer serializer, string fileName)
+        {
+            Serializer = serializer;
+            FileName = fileName;
+        }
 
         public void Write()
         {
             var directory = Environment.CurrentDirectory;
-            var fileName = $"trace_result.{Serializer.FileType()}";
+            var fileName = $"{FileName}.{Serializer.FileType()}";
             var filePath = $"{directory}/{fileName}";
             
             System.IO.File.WriteAllText(filePath, Serializer.Serialize());
